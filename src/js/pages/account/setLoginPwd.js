5ac8214ceaca772647f6cb78e47cc72ab0262f02
李德洋 <lidy@lideyangdeMacBook-Pro.local>
2017-11-07 21:30:42 +0800

    添加设置登录密码、成功页面

--- src/js/pages/account/setLoginPwd.js
+++ src/js/pages/account/setLoginPwd.js
@@ -1,62 +1,40 @@
 /**
- * Created by Lidy on 2017/11/7.
+ * Created by lidy on 2017/11/7.
  */
 +function () {
     var ForgetPwd = function () {//构造函数
-        this.$forgetPwdForm = $("#forgetPwdForm");
-        this.$getMobileCode = $("#getMobileCode");
+        this.$setLoginPwdForm = $("#setLoginPwdForm");
         this.validateForm = null;//验证方法
     }
     ForgetPwd.prototype = {
         init: function () {
             this.validate();//表单验证
-            this.initEvent();//绑定事件
-        },
-        initEvent: function () {
-            var self = this;
-            //发送验证码
-            this.$getMobileCode.on('click', function () {
-                if (self.validateForm.element($("#userMobile"))) {
-                    $(this).CountDown({
-                        data: {
-                            Mobile: $('#userMobile').val()
-                        },
-                        isMsg: true,
-                        isCallBack: 'message',//回调信息字段
-                        start: true,
-                        isSeed: true,
-                        isClick: false,
-                        url: '/mock/seedCode.json'
-                    });
-                }
-            });
         },
         validate: function () {
             //找回密码手机验证
-            this.validateForm = this.$forgetPwdForm.validate({
+            this.validateForm = this.$setLoginPwdForm.validate({
                 // 验证规则
                 rules: {
-                    userMobile: {
+                    loginPwd: {
                         required: true,
-                        isMobile: true
+                        minlength: 6
                     },
-                    code: {
+                    reLoginPwd: {
                         required: true,
-                        minlength: 7,
-                        maxlength: 7
+                        minlength: 6,
+                        equalTo: '#loginPwd'
                     }
                 },
                 // 设置错误信息
                 messages: {
-                    userMobile: {
-                        required: "请输入手机号",
-                        isMobile: "请输入正确的手机号码"
+                    loginPwd: {
+                        required: '请输入登录密码',
+                        minlength: '登录密码必须大于6位数'
                     },
-                    code: {
-                        required: "请输入验证码",
-                        isNumber: "请输入正确的验证码",
-                        minlength: "请输入7位验证码",
-                        maxlength: "验证码错误"
+                    reLoginPwd: {
+                        required: '请输入登录密码',
+                        minlength: '登录密码必须大于6位数',
+                        equalTo: '两次输入的密码必须一致'
                     }
                 },
                 // 错误信息显示
